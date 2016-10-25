f9789ed2caedb9152b06f1adcdab02528d11a33d
Tal Lannder <Tal@Pjili.com>
2016-10-25 22:54:56 +0000

    fmt

--- controllers/default.go
+++ controllers/default.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
 	"github.com/TalLannder/app1/models"
 	"github.com/astaxie/beego"
 )
@@ -12,48 +12,47 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-  user := new(models.User)
+	user := new(models.User)
 	user.Name = "Sponge Bob"
 	user.Age = 100
 
 	sess := c.GetSession("session")
 
 	if sess == nil {
-    b, err := json.Marshal(user)
-    if err != nil {
-        fmt.Println("error:", err)
-    }
-    newSession := make(map[string]interface{})
-    newSession["user"] = b
-    fmt.Printf("%#v\n", newSession)
+		b, err := json.Marshal(user)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		newSession := make(map[string]interface{})
+		newSession["user"] = b
+		fmt.Printf("%#v\n", newSession)
 		c.SetSession("session", newSession)
 	} else {
-	  c.Data["Session"] = sess
-  }
+		c.Data["Session"] = sess
+	}
 	c.TplName = "index.tpl"
 }
 
-
 type Cat struct {
-    Name string
-    Age  int
+	Name string
+	Age  int
 }
 
 func (c *MainController) GetCat() {
-  fmt.Println("In GetUserSession - start")
+	fmt.Println("In GetUserSession - start")
 
 	sess := c.GetSession("session")
 
-  cat := new(Cat)
+	cat := new(Cat)
 
-  if v, ok := sess.(map[string]interface{})["user"].([]byte); ok {
-    err := json.Unmarshal(v, &cat)
-    if err != nil {
-        fmt.Println("error:", err)
-    }
-  }
+	if v, ok := sess.(map[string]interface{})["user"].([]byte); ok {
+		err := json.Unmarshal(v, &cat)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+	}
 
-  fmt.Printf("%#v\n", cat)
+	fmt.Printf("%#v\n", cat)
 	c.Data["Cat"] = cat
 	c.TplName = "GetCat.tpl"
 }
